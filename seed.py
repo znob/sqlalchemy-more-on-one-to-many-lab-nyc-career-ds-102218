e0756ba319acf17b9964a644314480bee4869557
tkoar <terrancekoar@gmail.com>
2018-07-11 17:17:32 -0400

    added further instruction on how to read csv

--- seed.py
+++ seed.py
@@ -1,12 +1,30 @@
 from models import *
 from sqlalchemy import create_engine
+import pandas as pd
 
 engine = create_engine('sqlite:///actors.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# read the csv files to create the data you will need to create the players
-# hint: pandas
+# below we are reading the csv files to create the data we will need to create the players
+# pandas returns a DataFrame object from reading the CSV
+# we then tell the DataFrame object to turn each row into dictionaries
+# by giving to_dict the argument "orient='records'"
+# we are telling our DataFrame to make each row a dictionary using the column headers
+# as the keys for the key value pairs in each new dictionary
+# feel free to uncomment lines 18-21 to see each step of the process in your terminal
+# ____ example ______
+# la_dodgers0 = pd.read_csv('la_dodgers_baseball.csv')
+# la_dodgers1 = pd.read_csv('la_dodgers_baseball.csv').to_dict()
+# la_dodgers2 = pd.read_csv('la_dodgers_baseball.csv').to_dict(orient='records')
+# import pdb; pdb.set_trace()
+# __________________
+la_dodgers = pd.read_csv('la_dodgers_baseball.csv').to_dict(orient='records')
+la_lakers = pd.read_csv('la_lakers_basketball.csv').to_dict(orient='records')
+ny_yankees = pd.read_csv('ny_yankees_baseball.csv').to_dict(orient='records')
+ny_knicks = pd.read_csv('ny_knicks_basketball.csv').to_dict(orient='records')
 
+
+# now that we have the data for each player
 # add and commit the players, teams, sports and cities below
