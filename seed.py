505aa71f9030232aec8ea3f5a0ab60d6461b925c
tkoar <terrancekoar@gmail.com>
2018-07-11 16:35:18 -0400

    updated seed file instructions

--- seed.py
+++ seed.py
@@ -6,4 +6,7 @@ engine = create_engine('sqlite:///actors.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# read the csv files to create the data you will need to create the players
+# hint: pandas
+
 # add and commit the players, teams, sports and cities below
